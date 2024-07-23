3009c31cc69f9834d49e7dc03cbb0f87f1cfbe5d
Sid Premkumar <sid.premkumar@gmail.com>
2024-07-23 16:52:12 -0400

    add worker support on server

--- server/src/optimodel_server/cli.py
+++ server/src/optimodel_server/cli.py
@@ -2,5 +2,14 @@ import uvicorn
 from .index import app
 
 
+import argparse
+
+
 def main():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    parser = argparse.ArgumentParser(description="Run the Uvicorn server.")
+    parser.add_argument(
+        "--workers", type=int, default=1, help="Number of worker processes"
+    )
+    args = parser.parse_args()
+
+    uvicorn.run(app, host="0.0.0.0", port=8000, workers=args.workers)
